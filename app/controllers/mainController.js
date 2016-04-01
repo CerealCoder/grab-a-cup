f8c4823d5b9f21772474649674fc0e8b97ba4405
MilkshakePanda <iam.superpeluche@gmail.com>
2016-04-01 12:08:49 +0100

    Add geocoding call to get coordinates

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,7 +1,7 @@
-var mainCtrl = myApp.controller('mainCtrl', ['$scope', 'forecastInfo', '$location', function ($scope, forecastInfo, $location) {
+var mainCtrl = myApp.controller('mainCtrl', ['$scope', 'forecastInfo', '$location', '$http',function ($scope, forecastInfo, $location, $http) {
 
     // get the value of ng-model
-    $scope.setCity = function() {
+    $scope.setCity   = function() {
         forecastInfo.city = $scope.city
         $location.path('/map')
 
@@ -10,6 +10,31 @@ var mainCtrl = myApp.controller('mainCtrl', ['$scope', 'forecastInfo', '$locatio
 
     $scope.clearForm = function() {
         $scope.city = ''
+    }
+
+    $scope.getCoords = function(position) {
+
+        var lat = position.coords.latitude
+        var lng = position.coords.longitude
+        var coords = lat + ',' + lng
+        console.log(coords)
+    }
+
+    $scope.getPos    = function() {
+
+        navigator.geolocation.getCurrentPosition($scope.getCoords)
+
+    }
+
+    $scope.geoCode   = function() {
+
+        var city = $scope.city
+
+        $http.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ city +'&key=AIzaSyAE5RY82oagtBboJyPfz_YKDpayrVRVcdc&language=en').then(function(response) {
+            console.log(response.data)
+        }, function(error) {
+            console.log(error)
+        })
     }
 
 }])
